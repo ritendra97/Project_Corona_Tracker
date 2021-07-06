7538ae17af70194e6e34893b8173053cbf1e638d
Ritendra Pratap Singh <53062734+ritendra97@users.noreply.github.com>
2021-07-06 12:53:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//class base componenet
+
+import react from 'react';
+
+class App extends React.Component {
+
+    render(){
+
+        return(
+            <div> 
+               <h>Hello this is RItendra </h> 
+            </div>
+        )
+    }
+} 
+
+export default App;
